4bc071442bb7146b747983ba0f09b940344c2e88
Angelos-PC\Angelos <Angelos.Loucas@web.de>
2017-05-17 01:21:23 +0200

    Added Sat solver

--- src/grain.java
+++ src/grain.java
@@ -1,28 +1,47 @@
 
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.IProblem;
+import org.sat4j.specs.ISolver;
+
 public class grain {
 
 	boolean[] LFSR = new boolean[80];
 	boolean[] NFSR = new boolean[80];
+
+	final int MAXVAR = 1000000;
+	final int NBCLAUSES = 500000;
+
+	ISolver solver = SolverFactory.newDefault();
+
+	// prepare the solver to accept MAXVAR variables. MANDATORY for MAXSAT solving
+
+	
 	String key = "00000001001000110100010101100111100010011010101111001101111011110001001000110100";
 	String IV = "0000000100100011010001010110011110001001101010111100110111101111";
 
-	public void clockLfsr() {
-		boolean feedback = LFSR[62] ^ LFSR[51] ^ LFSR[38] ^ LFSR[23] ^ LFSR[13]
-				^ LFSR[0];
-		for (int i = 0; i < LFSR.length - 1; i++) {
-			LFSR[i] = LFSR[i + 1];
-		}
+	
+	int[] test1 = {1,2,3};
+	int[] test2 = {-1,2,-3};
+	int[] test3 = {1,-2,3};
+	
 
-		LFSR[LFSR.length - 1] = feedback;
-	}
-
-	public void clockNfsr() {
-		boolean feedback = LFSR[0] ^ g();
-		for (int i = 0; i < NFSR.length - 1; i++) {
-			NFSR[i] = NFSR[i + 1];
-		}
-		NFSR[79] = feedback;
-	}
+//	public void clockLfsr() {
+//		boolean feedback = LFSR[62] ^ LFSR[51] ^ LFSR[38] ^ LFSR[23] ^ LFSR[13]
+//				^ LFSR[0];
+//		for (int i = 0; i < LFSR.length - 1; i++) {
+//			LFSR[i] = LFSR[i + 1];
+//		}
+//
+//		LFSR[LFSR.length - 1] = feedback;
+//	}
+//
+//	public void clockNfsr() {
+//		boolean feedback = LFSR[0] ^ g();
+//		for (int i = 0; i < NFSR.length - 1; i++) {
+//			NFSR[i] = NFSR[i + 1];
+//		}
+//		NFSR[79] = feedback;
+//	}
 
 	// function g
 	public boolean g() {
@@ -83,17 +102,17 @@ public class grain {
 
 			NFSR[79] = feedbackNFSR;
 			LFSR[79] = feedbackLFSR ^ keyStream;
-			
-			if(keyStream)
+
+			if (keyStream)
 				System.out.print("1 ");
 			else
 				System.out.print("0 ");
 		}
-		
+
 		System.out.println();
 	}
-	
-	public void encrypt(){
+
+	public void encrypt() {
 		boolean feedbackLFSR = LFSR[62] ^ LFSR[51] ^ LFSR[38] ^ LFSR[23]
 				^ LFSR[13] ^ LFSR[0];
 		boolean feedbackNFSR = LFSR[0] ^ g();
@@ -108,8 +127,8 @@ public class grain {
 
 		NFSR[79] = feedbackNFSR;
 		LFSR[79] = feedbackLFSR;
-		
-		if(keyStream)
+
+		if (keyStream)
 			System.out.print("1 ");
 		else
 			System.out.print("0 ");
@@ -130,15 +149,17 @@ public class grain {
 		for (int i = IV.length(); i < LFSR.length; i++) {
 			LFSR[i] = true;
 		}
-		
+
 		clockBasis();
 
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		grain g = new grain();
 		for (int i = 0; i < 80; i++) {
 			g.encrypt();
 		}
 	}
+	
+	
 }
